606f7ef970e2876660df230ec3f1ee3f6571db09
ชื่Armyosung <supamad125@gmail.com>
2026-09-20 10:00:45 -0700

    upd-3

--- js/menu.js
+++ js/menu.js
@@ -13,3 +13,18 @@
                 });
             }
         });
+   // โหลด menu1.js
+   fetch("footer.html")
+   .then(res => res.text())
+   .then(data => {
+       document.getElementById("footer").innerHTML = data;
+
+       // ✅ re-bind toggle หลังจากเมนูถูก inject
+       const sidebarToggle = document.body.querySelector('#sidebarToggle');
+       if (sidebarToggle) {
+           sidebarToggle.addEventListener('click', event => {
+               event.preventDefault();
+               document.body.classList.toggle('sb-sidenav-toggled');
+           });
+       }
+   });
